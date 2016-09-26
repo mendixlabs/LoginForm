74b31b7b12438db274992a16a79579b69004c01b
aramlawiCap <adnan.ramlawi@capgemini.com>
2016-09-26 16:47:27 +0200

    added: Clear password field
    added clear password field functionality on failed login attempt.

--- src/LoginForm/widget/LoginForm.js
+++ src/LoginForm/widget/LoginForm.js
@@ -1,5 +1,5 @@
 /*jslint browser: true, devel:true, nomen:true, unparam:true, plusplus: true, regexp: true*/
-/*global logger, mendix, define, mx, dojo*/
+/*global logger, mendix, define, mx, dojo, require*/
 /**
  LoginForm
  ========================
@@ -33,7 +33,7 @@ define([
              template, templateWithoutShowPassword) {
     "use strict";
     // Declare widget.
-    return declare ("LoginForm.widget.LoginForm", [ _WidgetBase, _TemplatedMixin ], {
+    return declare("LoginForm.widget.LoginForm", [ _WidgetBase, _TemplatedMixin ], {
 
         // Template path, set in the postMixInProperties function
         templateString: "",
@@ -68,6 +68,7 @@ define([
          * Behaviour
          */
         showprogress: false,
+        clearPw: false,
         forgotmf: "",
         dofocus: false,
         showLoginFailureWarning: false,
@@ -218,6 +219,9 @@ define([
 
             dojoHtml.set(this.alertMessageNode, message);
             domClass.remove(this.alertMessageNode, "hidden");
+            if (this.clearPw) {
+                domAttr.set(this.passwordInputNode, "value", "");
+            }
         },
         /**
          * Retrieves the matching value from the internationalization object
@@ -268,8 +272,8 @@ define([
                 this.togglePasswordVisibility();
             }
 
-            var username = this._changeCase(this.usernameInputNode.value);
-            var password = this.passwordInputNode.value;
+            var username = this._changeCase(this.usernameInputNode.value),
+                password = this.passwordInputNode.value;
 
             if (username && password) {
                 if (this.showprogress) {
@@ -349,7 +353,7 @@ define([
             if (this.convertCase === "toUpperCase") {
                 return username.toUpperCase();
             }
-            if  (this.convertCase === "toLowerCase") {
+            if (this.convertCase === "toLowerCase") {
                 return username.toLowerCase();
             }
             return username;
